0f73c107cef3a0cd866f1726064c5af1d00be8fc
Jayaseelan <seelanjaya444@gmail.com>
2025-08-14 12:34:29 +0530

    Add files via upload

---
+++ Intermediate/Day 17 - Quiz Project/main.py
@@ -0,0 +1,20 @@
+# ================================  DAY 17 PROJECT - QUIZ - OOP ==================================
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_questions():
+    quiz.next_question()
+print("U completed the quiz")
+print(f"Ur final score was {quiz.score}/{quiz.question_num}")
+
---
+++ Intermediate/Day 17 - Quiz Project/question_model.py
@@ -0,0 +1,6 @@
+class Question:
+    def __init__(self, text_q, answer_q):
+        self.text = text_q
+        self.answer = answer_q
+
+   
